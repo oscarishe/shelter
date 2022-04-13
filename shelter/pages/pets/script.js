70b9f25e1a2cc664b1bcb0b9d4e84c848ee0a17a
oscarishe <a.polityko@mail.ru>
2022-04-13 21:20:51 +0300

    feat: add adaptive layout to our pets page

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -34,3 +34,24 @@ const maxPage = () => {
 	document.getElementById('button_minus').disabled=false;
 	document.getElementById('button_min').disabled=false;	
 }
+const closeBurgerMenu =() => {
+	document.getElementsByClassName('hidden_menu')[0].style.display="none";
+	document.getElementsByClassName('logo')[0].style.visibility="visible"
+	burger.className="burger_hidden";
+}
+window.onload = function() {
+var burger = document.getElementById('burger');
+burger.addEventListener("click", function(){
+  	if(burger.className=="burger_hidden") {
+  		document.getElementsByClassName('hidden_menu')[0].style.display="block";
+  		document.getElementsByClassName('logo')[0].style.visibility="hidden";
+  		burger.className="burger_active";
+  	}
+  	else if (burger.className=="burger_active") {
+  		document.getElementsByClassName('hidden_menu')[0].style.display="none";
+  		document.getElementsByClassName('logo')[0].style.visibility="visible"
+  		burger.className="burger_hidden";
+  	}
+
+});
+}
